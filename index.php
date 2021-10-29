f389ce7febc8948276a40f4928c498e8c4b1c517
Ed <edouard.liegeon@gmail.com>
2021-10-29 09:48:18 +0200

    architecture mvc + début de code php

--- index.php
+++ index.php
@@ -1,38 +1,45 @@
-<?php include("nav.php") ?>
-        <section class="page-section clearfix">
-            <div class="container">
-                <div class="intro">
-                    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/code.jpg" alt="Screenshot d'un code" />
-                    <div class="intro-text left-0 text-center bg-faded p-5 rounded">
-                        <h2 class="section-heading mb-4">
-                            <span class="section-heading-lower">Du commerce au Web</span>
-                        </h2>
-                        <p class="mb-3">Après 10 ans passés dans la grande distribution, des problèmes de santé m'ont permit de réfléchir à une reconversion. 
-                            J'ai donc décidé de me lancer dans le web, cherchant un métier où la créativité est de mise. <br>
-                           J'ai ainsi pu réaliser un titre professionnel <b>"Développeur web junior"</b>, mais à la fin de cette formation j'ai su que ce n'était que la première étape 
-                           d'un périple enrichissant où je savais vraiment où aller. Pour moi la polyvalence est importante et même essentielle, j'ai donc décider de rejoindre la formation PHP/Symfony.</p>
-                        <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="contact.php">Me contacter</a></div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="page-section cta">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-9 mx-auto">
-                        <div class="cta-inner bg-faded text-center rounded">
-                            <h2 class="section-heading mb-4">
-                                <span class="section-heading-lower">Mes projets</span>
-                            </h2>
-                            <p class="mb-3">Ici vous pourrez trouver les différents projets que j'ai réaliser dans mes différentes formations mais surtout de Openclassrooms. J'ai pu travailler sur JavaScript, PHP, Twig, Symfony, Sass. Je pourrai aussi 
-                                partager ma veille technologique. Vous pouvez donner votre avis et vos impressions sur mes expériences.
-                            </p>
-                            <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="projects.php">Accéder à mes posts</a></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <?php include("footer.php") ?>
-    </body>
-</html>
+
+<?php 
+require('controller/frontend.php');
+
+if (isset($_GET['action'])){
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    }
+    elseif ($_GET['action'] == 'post'){
+        if (isset($_GET['id']) && $_GET['id'] > 0){
+            post();
+        }
+        else {
+            throw new Exception('aucun identifiant de post envoyé');
+        }
+    }
+    elseif($_GET['action'] == 'addComment'){
+        if(isset($_GET['id']) && $_GET['id'] > 0){
+            if(!empty($_POST['auteur']) && !empty($_POST['contenu'])){
+                addComment($_GET['id'], $_POST['auteur'], $_POST['contenu']);
+            }
+            else{
+                echo 'Erreur : tous les champs ne sont pas remplis !';
+            }
+        }
+        else{
+            throw new Exception('aucun identifiant de billet envoyé');
+        }
+    }
+    elseif($_GET['action'] == 'contact'){
+        contact();
+    }
+    elseif($_GET['action'] == 'login'){
+        login();
+    }
+    elseif($_GET['action'] == 'register'){
+        register();
+    }
+    elseif($_GET['action'] == 'admin'){
+        admin();
+    }
+}
+else{
+    home();
+}
