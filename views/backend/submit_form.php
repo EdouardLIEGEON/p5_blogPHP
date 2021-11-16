6bdbdcf11fd25d52906d51cf8a000f8cef5ed919
Ed <edouard.liegeon@gmail.com>
2021-11-16 17:55:10 +0100

    Poursuite du formulaire

--- views/backend/submit_form.php
+++ views/backend/submit_form.php
@@ -1,15 +1,28 @@
 <?php
 
-if (!isset($_POST['name'])){
-    echo "Entrez un Pseudo d'utilisateur svp";
-    return;
-}
-if(!isset($_POST['password'])){
-    echo "Entrez un mot de passe svp";
-    return;
-}
-
 $name = $_POST['name'];
 $password = $_POST['password'];
+
+function verif_data($name, $password){
+    $name = trim($name);
+    $name = htmlspecialchars($name);
+    $name = stripslashes($name);
+    $password = trim($password);
+    $password = htmlspecialchars($password);
+    $password = stripslashes($password);
+}
+
+
+if (!isset($_POST['name']) || !isset($_POST['password'])){
+    echo "Entrez un Pseudo d'utilisateur et/ou un mot de passe svp";
+    return;
+}
+elseif(isset($_POST['name']) || isset($_POST['password'])){
+    verif_data($name, $password);
+    return preg_match([^A-Za-z0-9]);
+}
+
+
+
 
 ?>
