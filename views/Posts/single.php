bcfcc60aa746ad2c86d6bcdfaeb41217069da0ac
Ed <edouard.liegeon@gmail.com>
2022-03-18 10:23:29 +0100

    Codacy

--- views/Posts/single.php
+++ views/Posts/single.php
@@ -5,16 +5,16 @@
                 <div class="row">
                     <div class="col-xl-9 col-lg-10 mx-auto ">
                         <div class="bg-faded rounded p-5 m-auto">
-                            <p><?= $post->content ?></p>
-                            <em>Modifié le <?= $post->date ?></em><br><br>
-                            <p>Auteur : <?= $post->author ?></p>
+                            <p><?= htmlentities($post->content) ?></p>
+                            <em>Modifié le <?= htmlentities($post->date) ?></em><br><br>
+                            <p>Auteur : <?= htmlentities($post->author) ?></p>
                             <h3>Commentaires</h3><br>
                         <div class=row>
                             <div class="col-md-6">
                             <?php foreach($comments as $comment): ?>
-                                <p><strong><?= $comment->author ?></strong></p>
-                                <p><?= $comment->content ?></p>
-                                <span class="little_p">le <?= $comment->date ?></span><br>
+                                <p><strong><?= htmlentities($comment->author) ?></strong></p>
+                                <p><?= htmlentities($comment->content) ?></p>
+                                <span class="little_p">le <?= htmlentities($comment->date) ?></span><br>
                                 ____________________
                                 <?php endforeach ?>
                             </div>
@@ -22,7 +22,7 @@
 
                             <!--On vérifie que l'utilisateur est bien connecté-->
                                 <?php if(isset($_SESSION['user']) && !empty($_SESSION['user']['id'])){
-                                echo $form; }?> 
+                                print_r( $form); }?> 
                             </div>   
                         </div>
                     </div>
