b23133b0a5b41debb67aa9713909b26ed3965684
Ed <edouard.liegeon@gmail.com>
2022-03-18 11:32:48 +0100

    codacy

--- views/Posts/admin.php
+++ views/Posts/admin.php
@@ -7,13 +7,13 @@
                     <div class="col-md-6 mx-auto ">
                         <div class="bg-faded rounded p-5 m-auto" style="height: 1000px;">
                             <h3 class="section-heading mb-0">
-                                <span class="section-heading-upper"><?= htmlentities($post->title) ?></span>
+                                <span class="section-heading-upper"><?= htmlspecialchars($post->title) ?></span>
                             </h3><br>
-                            <p class="sousTitre"><?=htmlentities($post->subTitle) ?></p><br>    
-                            <p><?=htmlentities($post->content)?></p>    
-                            <p>Modifié le <?= htmlentities($post->date) ?></p>    
-                            <button class="btn-projects"><a href="/posts/modifier/<?=htmlentities($post->id) ?>">Modifier le projet</a></button><br><br>
-                            <button class="btn-projects"><a href="/posts/supprimer/<?php print_r(htmlentities($post->id)) ?>">Supprimer le projet</a></button>
+                            <p class="sousTitre"><?=htmlspecialchars($post->subTitle) ?></p><br>    
+                            <p><?=htmlspecialchars($post->content)?></p>    
+                            <p>Modifié le <?= htmlspecialchars($post->date) ?></p>    
+                            <button class="btn-projects"><a href="/posts/modifier/<?=htmlspecialchars($post->id) ?>">Modifier le projet</a></button><br><br>
+                            <button class="btn-projects"><a href="/posts/supprimer/<?=(htmlspecialchars($post->id)) ?>">Supprimer le projet</a></button>
                         </div>
                     </div>
                     <?php endforeach ?>
